3444e090417d94399ca35637d7c136d96e233100
Jasmine <jasminegit config --global user.email jasmine_singh1992@hotmail.comgit config --global user.name Your>
2020-02-11 22:26:01 +0000

    timelapse

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 
 
-for (let i = 0 ; i < sentence.length; i++) { 
-  
+for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
-    process.stdout.write(sentence.charAt(i));
-  }, 1000 * i) 
+    if (i === (sentence.length - 1)) {
+      process.stdout.write('\n');
+    } else {
+      process.stdout.write(sentence.charAt(i));
+    }
+  }, 50 * i)
 }
